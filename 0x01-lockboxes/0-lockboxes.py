1808ea811eb086eb572ea53bed442926be6464cb
Farad078 <faradokorie@gmail.com>
2023-07-06 19:12:06 +0100

    first commit

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes: list) -> bool:
+    """a function that unlock boxes.
+    Arg: Array of arrays
+    returns: boolean"""
+
+    # Empty array
+    openbox = [boxes[0]]
+
+    # Iterate through the open Box
+    for box in openbox:
+        for key in box:
+            if key <= len(boxes) - 1:
+                if boxes[key] not in openbox:
+                    openbox.append(boxes[key])
+    if len(openbox) == len(boxes):
+        return True
+    return False

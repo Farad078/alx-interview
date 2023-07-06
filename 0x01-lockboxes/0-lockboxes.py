0474edce555b8bc210a0006553f2d88c38007b47
Farad078 <faradokorie@gmail.com>
2023-07-06 23:41:14 +0100

    new update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,14 @@
-#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)  # Total number of boxes
+    unlocked = [False] * n  # Track the unlocked status of each box
+    unlocked[0] = True  # The first box is already unlocked
+    stack = [0]  # Start with the first box
 
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
 
-def listUnlockCheck(boxes):
-    if isinstance(boxes, list):
-        for box in boxes:
-            if isinstance(box, list):
-                for value in box:
-                    if isinstance(value, int):
-                        return True
-                    return False
-    return False
-
-
-def canUnlockAll(boxes: list) -> bool:
-    """a function that unlock boxes.
-    Arg: Array of arrays
-    returns: boolean"""
-    if listUnlockCheck(boxes):
-        # Empty array
-        openbox = [boxes[0]]
-
-        # Iterate through the open Box
-        for box in openbox:
-            for key in box:
-                if len(boxes) - 1 >= key >= 1 and isinstance(key, int):
-                    if boxes[key] not in openbox:
-                        openbox.append(boxes[key])
-        if len(openbox) == len(boxes):
-            return True
-    return False
-
+    return all(unlocked)
